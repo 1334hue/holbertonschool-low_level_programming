64546bb1abe589c4c4390d57640a150ace0b2a22
root <root@ip-10-42-222-16.ec2.internal>
2026-03-09 10:59:52 +0000

    Fix trailing

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ *
  * _puts_recursion - prints a string, followed by a new line
  * @s: the string to print
  *
@@ -15,4 +16,5 @@ void _puts_recursion(char *s)
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
-} 
+}
+s/\s\+$//e
