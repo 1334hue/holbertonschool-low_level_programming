908cedfb0f4c9e035bf30ac9ec009101985b225a
root <root@ip-10-42-240-119.ec2.internal>
2026-03-10 11:46:42 +0000

    Fix directory nesting for checker

---
+++ holbertonschool-low_level_programming/pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+
+#endif
