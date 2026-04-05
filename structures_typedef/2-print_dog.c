16ead22ef5dcb98fe95afb48227535ec9ada753e
dana <trivian1966@gmail.com>
2026-04-05 02:08:36 +0000

    betty coding style

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-
-	printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
