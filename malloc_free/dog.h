8f5e28863ca2c994b077ba40908fcd9982045162
dana <trivian1966@gmail.com>
2026-04-05 02:27:41 +0000

    Final Betty Fix

--- malloc_free/dog.h
+++ malloc_free/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - a dog's basic info
  * @name: First member
@@ -15,15 +14,12 @@ struct dog
 	float age;
 	char *owner;
 };
-
 /**
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
