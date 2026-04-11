02de26bb82b6712c4b923c36bc787f40d5f35edd
dana <trivian1966@gmail.com>
2026-04-11 08:22:57 +0000

    Fix Betty documentation for main function

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints the result of simple operations
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ *
+ * Return: 0 on success, or exit with error codes
+ */
 int main(int argc, char *argv[])
 {
 	int a, b;
