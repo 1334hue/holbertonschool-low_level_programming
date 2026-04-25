1428dfc1443266da74cbbdfbfa096861f463f1ae
dana <trivian1966@gmail.com>
2026-04-25 16:13:58 +0000

    Creat file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include "main.h"
+ #include "main.h"
  /**
   *  _strlen - returns the length of a string 
   * @s: the string whose length to check
@@ -19,23 +19,24 @@
  }
 
  /**
-  * append_text_to_file - appends text to file 
-  * @filename: name of file to create
+  * creat_file - creats a file
+  * @filename: name of file to creat
   * @text_content: text to write
   * 
   * Return: 1 on success 0 on failure
   */
- int append_text_to_file(const char *filename, char *text_content)
- {
-      int fd;
-      ssize_t bytes =0 , len = _strlen(text_content);
-      if (!filename)
-           return (-1);
-     fd = open(filename, O_WRONLY | O_APPEND );
-           if (fd == -1)
-           return(-1);
-           if (len)
-          bytes = write(fd, text_content, len);
-            close (fd);
-            return(bytes == len ? 1 : -1);
+
+  int creat_file(const char *filename, chat *text_content)
+  {
+int fd;
+ssize_t bytes =0 , len = _strlen(text_content);
+if (!filename)
+return (-1);
+fi = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR );
+if (fd == -1)
+return (-1);
+if (len)
+bytes = write(fd, text_content, len);
+close (fd);
+return(bytes == len ? 1 : -1);
   }
