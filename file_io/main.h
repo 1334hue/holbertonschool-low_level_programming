3536a5d032555a96f9d362bc9b81032c49af873b
dana <trivian1966@gmail.com>
2026-04-25 14:08:30 +0000

    editing main

--- file_io/main.h
+++ file_io/main.h
@@ -1,25 +1,16 @@
- #include "main.h"
- /**
-  * read_textfile - reads text from a file and prints it
-  * @filename: name of file to read
-  * @letters: number of bytes to read
-  * 
-  * Return: number of bytes read/printed
-  */
+#ifndef MAIN.H
+#define MAIN.H
 
-  ssize_t read_textfile(const char *filename , size_t letters)
-  {
-            int fd;
-            ssize_t bytes;
-            char buf[READ_BUF_SIZE * 8];
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-            if(!filename || !letters)
-               return(0);
-            fd = open(filename , O_RDONLY);
-            if (fd == -1)
-                 return(0);
-           bytes = read(fd, &buf[0], letters);
-           bytes = write(STDOUT_FILENO,&buf[0], bytes);
-           close(fd);
-           return(bytes);
-  }
+#define READ_BUF_SIZE 1024
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename , size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
